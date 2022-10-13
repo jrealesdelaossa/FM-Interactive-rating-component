de51c1bd7d48ee55ba3d8813aef1f83ffe560545
jrealesdelaossa <jrealesdelaossa@gmail.com>
2022-10-13 11:09:21 -0500

    change of color with button select

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ const btn3 = document.querySelector('.btn-3');
 const btn4 = document.querySelector('.btn-4');
 const btn5 = document.querySelector('.btn-5');
 
-const buttonSelect = document.querySelector('.btn')
-const buttons = [btn1, btn2, btn3, btn4, btn5];
+const btns = [btn1, btn2, btn3, btn4, btn5];
 
-function changeStatusSelection() {
-  buttonSelect.classList.toggle('btn-select');
-  for (let btn of buttons) {
-    console.log(btn.classList.contains('btn'));
+function changeSelectButton() {
+  for (let b of btns) {
+    if (b.classList.contains('btn-select')) {
+      b.classList.remove('btn-select');
+    }
   }
+
+  this.classList.add('btn-select')
 }
 
-btn1.addEventListener('click', changeStatusSelection);
-btn2.addEventListener('click', changeStatusSelection);
-btn3.addEventListener('click', changeStatusSelection);
-btn4.addEventListener('click', changeStatusSelection);
-btn5.addEventListener('click', changeStatusSelection);
+btn1.addEventListener('click', changeSelectButton);
+btn2.addEventListener('click', changeSelectButton);
+btn3.addEventListener('click', changeSelectButton);
+btn4.addEventListener('click', changeSelectButton);
+btn5.addEventListener('click', changeSelectButton);
