e4227b88046bdecd3356b8f1ac5367d0643b3fa7
jrealesdelaossa <jrealesdelaossa@gmail.com>
2022-10-13 12:32:16 -0500

    project finished

--- script.js
+++ script.js
@@ -21,3 +21,37 @@ btn2.addEventListener('click', changeSelectButton);
 btn3.addEventListener('click', changeSelectButton);
 btn4.addEventListener('click', changeSelectButton);
 btn5.addEventListener('click', changeSelectButton);
+
+/**
+ * Thank you state interaction
+ */
+
+const btnSelect = document.querySelector('.btn-submit');
+// contenedor
+const container = document.querySelector('#main-container');
+
+function getSelectButton() {
+  for (let b of btns) {
+    if (b.classList.contains('btn-select')) {
+      return b.innerHTML;
+    }
+  }
+}
+
+function changeAThanksPage() {
+  const value = getSelectButton();
+
+  // elimina los estilos de la pagina de selección
+  // y los reemplaza por los de la pagina de agradecimiento
+  container.classList.remove('main-container');
+  container.classList.add('main-container-thank');
+
+  container.innerHTML = `
+  <img src='./images/illustration-thank-you.svg' alt='thanks image'/>
+  <span>You selected ${value} out of 5</span>
+  <h1>Thank you!</h1>
+  <p>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
+  `;
+}
+
+btnSelect.addEventListener('click', changeAThanksPage);
